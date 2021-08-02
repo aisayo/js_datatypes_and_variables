42f7a619b46bebbd794d4b7b08c5e4b64401d52d
Aysan Isayo <aisayo@icloud.com>
2021-08-02 15:28:41 -0700

    solution

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 //---- DATA TYPES ----//
-// Go over different data types:
 // What are the different data types we can work with in JS?
 // 8 basic data types in JS:
 // String 
@@ -11,21 +10,32 @@
 // Null
 // Undefined
 
-// What are the three ways we can define a string?
+// What are the different ways we can define a string?
+// "Using double quotation marks"
+// 'Using single quotation marks'
+// `Using backticks so we can ${interpolate}`
 
-// console.log(typeof "Hello World!") //string
-// console.log(typeof 3) // number
-// console.log(typeof 3.14) // number
-// console.log(typeof {name: "timmy", breed: "Siamese"}) // object
-// console.log(typeof ['Hello', 'World']) // object gotcha! array is not a datatype, it is a data structure
-// console.log(typeof null) // object gotcha!
+// What are some examples of numbers
+// any integers, floats and NaN 
+
+// Booleans: identify things in JS that will return false
+// 0
+// NaN
+// undefined
+// false
+// null
+// ""
+
+// Everything else is true
+
+//---- USING TYPEOF ----// 
 // console.log(typeof undefined) // undefined
 // console.log(typeof true) // boolean
 // console.log(typeof Symbol("name")) // symbol
 // console.log(typeof typeof 900719925124740998n) 
-// console.log(typeof NaN) // number gotcha!
 
-// What is a primitive data type? a datatype that can only store a single piece of data
+// What is a primitive data type? 
+// a datatype that can only store a single piece of data
 
 // Of the data types we went over, which one is not considered a primitive data type
 // Object - non-primitive
@@ -35,15 +45,15 @@
 
 //---- OPERATORS ----//
 // What do we use to assign values? x = y (assignment)
-// x += y addition assignment
+// x += y addition assignment, what is happening here?
 // 1 + 1 // returns 2 
-// "1" + 1 // returns 11
+// "1" + 1 // returns 11, why is this happening?
 // 3 == '3' // returns true
 // 3 === '3' // returns false, strict equals
-// x = 3 ++x returns 4 x++ returns 3
+// x = 3: ++x returns 4 x++ returns 3
 
 //---- DATATYPE GOTCHAS ----// 
-
+// console.log(typeof ['Hello', 'World']) // object gotcha! array is not a datatype, it is a data structure
 // console.log(0 == false) // true
 // console.log(0 === false) // false
 // console.log("" == 0) // true 
@@ -52,6 +62,8 @@
 // show students Number(true)
 // console.log(false + false) // 0
 // show students Number(false)
+// console.log(typeof null) // object gotcha!
+// console.log(typeof NaN) // number gotcha!
 
 // Add/Subtract
 // console.log("Thing" + 1); // "Thing1"
@@ -61,13 +73,17 @@
 // what is a variable?
 // why do we use variables?
 
+//---- Let vs Const ----//
+// What is a good use case of let vs const?
+// What is the difference between let and const
+
 //---- Variable naming ----//
 // what is wrong with the following variable
 // let 1jedi = 'Yoda' // variables shouldn't start with numbers
 
 // fix these
 // let lukeskywalker = 'Luke Skywalker'
-// let break = 'Darth Vader' // SyntaxError: Unexpected token 'break' / reserved key
+// let function = 'Darth Vader' // SyntaxError: Unexpected token 'break' / reserved key
 // const counter = 0
 // console.log(++counter)
 

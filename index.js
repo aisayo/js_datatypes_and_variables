1f6ac52f45534766b095fd8a9d61df5cfd73e54a
Aysan Isayo <aisayo@icloud.com>
2021-08-02 14:03:55 -0700

    deliverables

--- index.js
+++
@@ -1,7 +0,0 @@
-// let name = "aysan"
-// let name = "bob"
-// SyntaxError: Identifier 'name' has already been declared
-
-// const cat = 'rose'
-// cat = 'timmy'
-// TypeError: Assignment to constant variable.
